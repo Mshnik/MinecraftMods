877b372f8656e95f86d73f8ff74cde3218bd1c70
Mshnik <Mgpshnik@gmail.com>
2019-10-09 20:46:30 -0700

    Move properties to static final field

--- src/main/java/com/minecraftmods/onemod/blocks/FirstBlock.java
+++ src/main/java/com/minecraftmods/onemod/blocks/FirstBlock.java
@@ -6,11 +6,13 @@ import net.minecraft.block.material.Material;
 
 /** @author Mshnik */
 final class FirstBlock extends Block {
+  private static final Properties PROPERTIES =
+      Properties.create(Material.IRON)
+          .sound(SoundType.METAL)
+          .hardnessAndResistance(2.0f)
+          .lightValue(14);
+
   FirstBlock() {
-    super(
-        Properties.create(Material.IRON)
-            .sound(SoundType.METAL)
-            .hardnessAndResistance(2.0f)
-            .lightValue(14));
+    super(PROPERTIES);
   }
 }
