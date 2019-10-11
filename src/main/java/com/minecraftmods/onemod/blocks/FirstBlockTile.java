a8083b912a4b1ca10463cfacee4099c7fbd68bd2
Mshnik <Mgpshnik@gmail.com>
2019-10-10 21:07:36 -0700

    Tutorial led cleanup

--- src/main/java/com/minecraftmods/onemod/blocks/FirstBlockTile.java
+++ src/main/java/com/minecraftmods/onemod/blocks/FirstBlockTile.java
@@ -17,7 +17,7 @@ import javax.annotation.Nullable;
 /** @author Mshnik */
 final class FirstBlockTile extends TileEntity implements ITickableTileEntity {
   private static final String INVENTORY_KEY = "inventory";
-  private ItemStackHandler handler;
+  private final LazyOptional<ItemStackHandler> handler = LazyOptional.of(this::createHandler);
 
   FirstBlockTile() {
     super(ObjectHolders.FIRST_BLOCK_TILE);
@@ -33,47 +33,43 @@ final class FirstBlockTile extends TileEntity implements ITickableTileEntity {
   @Override
   public void read(CompoundNBT compoundNBT) {
     CompoundNBT handlerCompoundNBT = compoundNBT.getCompound(INVENTORY_KEY);
-    getHandler().deserializeNBT(handlerCompoundNBT);
+    handler.ifPresent(h -> h.deserializeNBT(handlerCompoundNBT));
     super.read(compoundNBT);
   }
 
   @Override
   public CompoundNBT write(CompoundNBT compoundNBT) {
-    CompoundNBT handlerCompoundNBT = getHandler().serializeNBT();
-    compoundNBT.put(INVENTORY_KEY, handlerCompoundNBT);
+    handler
+        .map(ItemStackHandler::serializeNBT)
+        .ifPresent(handlerCompoundNBT -> compoundNBT.put(INVENTORY_KEY, handlerCompoundNBT));
     return super.write(compoundNBT);
   }
 
-  private ItemStackHandler getHandler() {
-    if (handler == null) {
-      handler =
-          new ItemStackHandler() {
-            @Override
-            public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
-              return stack.getItem() == Items.DIAMOND;
-            }
+  private ItemStackHandler createHandler() {
+    return new ItemStackHandler() {
+      @Override
+      public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+        return stack.getItem() == Items.DIAMOND;
+      }
 
-            @Nonnull
-            @Override
-            public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
-              if (!isItemValid(slot, stack)) {
-                return stack;
-              }
-              return super.insertItem(slot, stack, simulate);
-            }
-          };
-    }
-    return handler;
+      @Nonnull
+      @Override
+      public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+        if (!isItemValid(slot, stack)) {
+          return stack;
+        }
+        return super.insertItem(slot, stack, simulate);
+      }
+    };
   }
 
   @Nonnull
   @Override
   public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
     if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-      // T cast is safe here because ITEM_HANDLER_CAPABILITY is Capability<IItemHandler>.
-      return LazyOptional.of(() -> (T) getHandler());
+      return handler.cast();
+    } else {
+      return super.getCapability(cap, side);
     }
-
-    return super.getCapability(cap, side);
   }
 }
